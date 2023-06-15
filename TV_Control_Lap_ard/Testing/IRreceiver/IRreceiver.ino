62af7704ae3783d38e47656e20285db0aa430dcd
Mohamed Walid <moh.programs.1103@gmail.com>
2023-06-15 16:46:21 +0300

    Update IRreceiver.ino

--- TV_Control_Lap_ard/Testing/IRreceiver/IRreceiver.ino
+++ TV_Control_Lap_ard/Testing/IRreceiver/IRreceiver.ino
@@ -2,20 +2,16 @@
 
 # define IR_RECEIVE_PIN 2
 
-void setup(){
+void setup()
+{
 
-  IrReceiver.begin(IR_RECEIVE_PIN, DO_NOT_ENABLE_LED_FEEDBACK); // Start the receiver
+  IrReceiver.begin(IR_RECEIVE_PIN, DO_NOT_ENABLE_LED_FEEDBACK);
   Serial.begin(9600);
 }
 
 void loop() {
-
   if (IrReceiver.decode()) {
-      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
-      // USE NEW 3.x FUNCTIONS
-      IrReceiver.printIRResultShort(&Serial); // Print complete received data in one line
-      IrReceiver.printIRSendUsage(&Serial);   // Print the statement required to send this data
-      
-      IrReceiver.resume(); // Enable receiving of the next value
-  }  
+      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+      IrReceiver.resume(); 
+  }
 }
