51a31b7ccfe8cb61143ed0d1fd050513815ef678
Mohamed Walid <moh.programs.1103@gmail.com>
2023-06-15 16:46:35 +0300

    Update main.py

--- TV_Control_Lap_py/main.py
+++ TV_Control_Lap_py/main.py
@@ -1,3 +1,11 @@
-import AppOpener
+import serial
 
-AppOpener.mklist()
+port = "com16"
+baudRate = 9600
+
+ard = serial.Serial(port="com16", baudrate=9600)
+while True:
+   code_ = ard.readline().decode(encoding="utf-8").strip()
+   print(code_)
+   print(type(ard.readline()))
+   print(type(ard.readline().decode(encoding="utf-8")))
