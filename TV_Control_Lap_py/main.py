6827c2f4509b9ec8e41c1596bf302de9169ed35b
Mohamed Walid <moh.programs.1103@gmail.com>
2023-06-16 09:53:09 +0300

    Delete main.py

--- TV_Control_Lap_py/main.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-port = "com16"
-baudRate = 9600
-
-ard = serial.Serial(port="com16", baudrate=9600)
-while True:
-    output = ard.readline().decode(encoding="utf-8").strip()
-    print(output)
